e5d660286bc843921e58131f9817afc2a0678248
Luan Menezes <luanmenezes99@gmail.com>
2021-01-17 15:00:50 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="app">
+    <h1 :style="{ color }">My List</h1>
+    <input type="text" v-model="currentFood" @keyup.enter="addFood" />
+    <button @click="addFood">Add</button>
+    <food-list :foods="foods" @stop="handleStop" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import FoodList from "./components/FoodList.vue";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  components: { FoodList },
+  data: () => ({
+    foods: ["Apple", "Cheese", "Wine", "Milk"],
+    currentFood: "",
+    color: "tomato"
+  }),
+  methods: {
+    addFood() {
+      this.foods.push(this.currentFood);
+      this.currentFood = "";
+    },
+    handleStop(payload) {
+      window.alert(payload);
+    }
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  display: flex;
+  justify-content: center;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 50%;
+  margin: 0 4rem;
+}
+
+input {
+  height: 1.5rem;
+}
+
+button {
+  background-color: rebeccapurple;
+  color: aliceblue;
+  outline: none;
+  border: solid 1px rebeccapurple;
+  height: 2rem;
+  width: 100%;
+  margin: 0.25rem 0;
 }
 </style>
---
+++ src/components/FoodList.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul>
+    <li v-for="food in foods" :key="food">{{ food }}</li>
+  </ul>
+</template>
+
+<script>
+const MAX_FOODS = 5;
+
+export default {
+  props: {
+    foods: { type: Array, default: () => [] }
+  },
+  watch: {
+    foods() {
+      if (this.foods.length >= MAX_FOODS) {
+        this.$emit("stop", "Tá cheio");
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  margin: 1rem 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+li {
+  padding: 0.25rem;
+  border: salmon 1px solid;
+  margin: 0.5rem 0;
+}
+</style>
